eef5852d374c23526214ddf0865c72b494fe601d
hikmet Nuri melemen <hikmet.nuri.33@gmail.com>
2022-04-15 20:56:04 +0300

    Vücüd kitle endexi yazıldı

---
+++ src/KitleEndexi.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class KitleEndexi {
+    public static void main(String[] args) {
+
+     Scanner input=new Scanner(System.in);
+        System.out.println("Lütfen kilonuzu giriniz");
+        double kilo= input.nextDouble();
+        System.out.println("Lütfen boyunuzu (santimetre cinsinde) giriniz");
+        double boy= input.nextDouble();
+        double endeks=kilo/(boy*boy);
+        System.out.println("Kilo endeksiniz: "+endeks);
+    }
+}
