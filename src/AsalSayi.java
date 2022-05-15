4e21a2ec66dffee5d4f1a4c9dfba5df22457eb68
hikmet Nuri melemen <hikmet.nuri.33@gmail.com>
2022-05-15 14:16:06 +0300

    swich case ile hesap makinesi yaptım

---
+++ src/AsalSayi.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class AsalSayi
+{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+          System.out.println("Kontrol etmek istediğiniz asal sayıyı girin");
+          int sayi1=sc.nextInt();
+          int sayac=0;
+
+      }
+    }
+
+
